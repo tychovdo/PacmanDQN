8ae3bfe317113d48385713e1c55f74354ceff21a
Tycho van der Ouderaa <tychovdo@gmail.com>
2016-06-13 20:07:31 +0200

    runfile and saves readme

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Total number of episodes:          1000000
+# Total number of training episodes: 100000
+# Level:                             mediumClassic
+
+python3 pacman.py -p AlphaAgent -n 1000000 -x 100000 -l mediumClassic
